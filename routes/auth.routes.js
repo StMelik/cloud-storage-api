f96dfb503bfb354335813c461b8c45fc93676158
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-09-03 04:35:27 +0300

    feat: добавить авторизацию

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const { check, validationResult } = require('express-validator')
 
 router.post('/sign-up',
@@ -25,10 +27,53 @@ router.post('/sign-up',
                     .json({ message: 'Такой пользователь уже существует' })
             }
 
-            const hashPassword = await bcrypt.hash(password, 15)
+            const hashPassword = await bcrypt.hash(password, 10)
             const user = await User.create({ email, password: hashPassword })
 
             return res.json({ message: 'Пользователь создан' })
+
+        } catch (e) {
+            console.log(e);
+            res.send({ message: 'Server error' })
+        }
+
+    })
+
+router.post('/sign-in',
+
+    async (req, res) => {
+        try {
+            const { email, password } = req.body
+            const user = await User.findOne({ email })
+            if (!user) {
+                return res
+                    .status(400)
+                    .json({ message: 'Неверный логин или пароль' })
+            }
+
+            const isPassValid = bcrypt.compareSync(password, user.password)
+            if (!isPassValid) {
+                return res
+                    .status(400)
+                    .json({ message: 'Неверный логин или пароль' })
+            }
+
+            const token = jwt.sign(
+                { id: user.id },
+                config.get('secretKey'),
+                { expiresIn: "1d" }
+            )
+
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
 
         } catch (e) {
             console.log(e);
